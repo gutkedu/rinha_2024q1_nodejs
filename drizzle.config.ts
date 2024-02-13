f39188cedf695f98d4360b2d978bd005ece994df
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2024-02-13 17:19:15 -0300

    fix: remove e2e tests after failed attempts with drizzle orm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,10 @@ export default {
   out: './drizzle/migrations',
   driver: 'pg',
   dbCredentials: {
-    host: process.env.DB_HOST as string,
+    host: process.env.DB_HOST ?? 'localhost',
     port: parseInt(`${process.env.DB_PORT || '5432'}`),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE as string,
+    database: process.env.DB_DATABASE ?? 'rinha_api',
   },
 } satisfies Config
